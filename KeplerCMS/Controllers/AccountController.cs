48eb730a7879ee2c531a09ed70173552829c3806
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2020-04-21 19:46:26 +0200

    Inital login working

---
+++ KeplerCMS/Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using KeplerCMS.Models;
+using KeplerCMS.Data;
+using Isopoh.Cryptography.Argon2;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
+namespace KeplerCMS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger, DataContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(string username, string password)
+        {
+            var users = _context.Users.ToList();
+            if(username != null && password != null)
+            {
+                var user = _context.Users.Where(user => user.Username.ToLower() == username.ToLower()).FirstOrDefault();
+                if (user != null && Argon2.Verify(user.Password, password))
+                {
+
+                    var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                };
+
+                    var claimsIdentity = new ClaimsIdentity(
+                        claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var authProperties = new AuthenticationProperties
+                    {
+                        #region cookieoptions
+                        /*AllowRefresh = true,
+                        //
+                        // Refreshing the authentication session should be allowed.
+
+                        //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(20),
+                        // The time at which the authentication ticket expires. A 
+                        // value set here overrides the ExpireTimeSpan option of 
+                        // CookieAuthenticationOptions set with AddCookie.
+
+                        //IsPersistent = true,
+                        // Whether the authentication session is persisted across 
+                        // multiple requests. When used with cookies, controls
+                        // whether the cookie's lifetime is absolute (matching the
+                        // lifetime of the authentication ticket) or session-based.
+
+                        //IssuedUtc = <DateTimeOffset>,
+                        // The time at which the authentication ticket was issued.
+
+                        //RedirectUri = <string>
+                        // The full path or absolute URI to be used as an http 
+                        // redirect response value.*/
+                        #endregion
+                    };
+
+                    HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(claimsIdentity),
+                        authProperties);
+                    return RedirectToAction("index", "home");
+                }
+                else
+                {
+
+                    ViewData["wrong_users_or_password"] = true;
+                    return View();
+                }
+            } else
+            {
+                ViewData["please_enter_username"] = true;
+                return View();
+            }
+        }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ KeplerCMS/Data/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeplerCMS.Data.Models
+{
+    [Table("users")]
+    public class Users
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("username")]
+        public string Username { get; set; }
+        [Column("password")]
+        public string Password { get; set; }
+        [Column("figure")]
+        public string Figure { get; set; }
+    }
+}
