36fc3a7d607c6f2e93970f4667e11e1ac9d550ff
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2020-05-19 22:29:55 +0200

    Added footer, new banners that needs to implemented and endpoint for furni purchase, some new js

---
+++ Controllers/FurniPurchaseController.cs
@@ -0,0 +1,42 @@
+﻿using Isopoh.Cryptography.Argon2;
+using KeplerCMS.Data;
+using KeplerCMS.Filters;
+using KeplerCMS.Models;
+using KeplerCMS.Services;
+using KeplerCMS.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace KeplerCMS.Controllers
+{
+    public class FurniPurchaseController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly ICreditService _creditService;
+
+        public FurniPurchaseController(IUserService userService, ICreditService creditService)
+        {
+            _userService = userService;
+            _creditService = creditService;
+        }
+
+        [LoggedInFilter(false)]
+        [HttpPost]
+        [Route("furnipurchase/purchase_confirmation")]
+        public async Task<IActionResult> PurchaseConfirmation()
+        {
+            // TODO: Actually check if the user is logged in, look up catalogitem, then check if the user has enough credits, give the user the item 
+            //and queue update in command queue - Otherwise let the user know that they dont have enough coins. Or det item has been delisted/hidden.
+            return Content("Hah ... This will work eventually.. I have a shit ton of other shit to deal with.");
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,3 +7,26 @@
             updateHabboCreditAmounts(msg);
     });
 }
+j(document).ready(function () {
+    j(".buy-btn").click(function (e) {
+        e.preventDefault();
+        var dialog = createDialog("purchase_dialog", "Bekræft køb", 9001, 0, -1000, closePurchase);
+        appendDialogBody(dialog, "<p style=\"text-align:center\"><img src=\"/images/progress_bubbles.gif\" alt=\"\" width=\"29\" height=\"5\" /></p><div style=\"clear\"></div>", true);
+        moveDialogToCenter(dialog);
+        showOverlay();
+        new Ajax.Request(
+            habboReqPath + "/furnipurchase/purchase_confirmation",
+            {
+                method: "post", parameters: "product=" + j(this).data("product"), onComplete: function (req, json) {
+                    setDialogBody(dialog, req.responseText);
+                }
+            });
+
+    });
+
+    j(".roomForward").click(function (e) {
+        e.preventDefault();
+        roomForward(this, j(this).data("roomid"), j(this).data("type"), false); return false;
+
+    });
+});
