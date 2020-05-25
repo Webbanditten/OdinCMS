79833241d2a5a37e9d913dbbdc47fd84876fe598
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2020-05-25 21:03:51 +0200

    Room forwarding

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -15,10 +15,12 @@ namespace KeplerCMS.Controllers
             _userService = userService;
         }
         [LoggedInFilter]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int forwardId = 0, int roomId = 0)
         {
             var userWithSSO = await _userService.GenerateSSO(int.Parse(User.Identity.Name));
             ViewData["sso"] = userWithSSO.SSOTicket;
+            ViewData["forwardId"] = forwardId;
+            ViewData["roomId"] = roomId;
             return View();
         }
 
