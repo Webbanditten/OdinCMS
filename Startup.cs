5f770ef385bab66d362dd6f4c5158fc46bfb8ebb
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2021-01-04 20:35:52 +0100

    Added dev error page

--- Startup.cs
+++ Startup.cs
@@ -113,7 +113,8 @@ namespace KeplerCMS
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                //app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
