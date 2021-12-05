8c9c8d109d477df3fe4891d099e605283f60d420
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2021-12-05 20:35:46 +0100

    feat: Add implement cms.use_ruffle so that it possible to disable if we want to do that!

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using KeplerCMS.Filters;
 using KeplerCMS.Services.Interfaces;
+using System.Threading.Tasks;
 
 namespace KeplerCMS.Controllers
 {
@@ -9,17 +10,18 @@ namespace KeplerCMS.Controllers
     public class ProfileController : Controller
     {
         private readonly IUserService _userService;
+        private readonly ISettingsService _settingService;
 
-        public ProfileController(IUserService userService)
+        public ProfileController(IUserService userService, ISettingsService settingService)
         {
+            _settingService = settingService;
             _userService = userService;
         }
 
         [LoggedInFilter]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-            
+            return View(await _settingService.GetAll());   
         }
 
         [LoggedInFilter]
