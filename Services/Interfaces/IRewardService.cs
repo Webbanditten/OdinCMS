40cacd799a0a3a36a1db11d4425d80f41de005ba
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2022-11-27 21:55:19 +0100

    feat: added xmas2006 calendar

---
+++ Services/Interfaces/IRewardService.cs
@@ -0,0 +1,12 @@
+﻿using KeplerCMS.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KeplerCMS.Services.Interfaces
+{
+    public interface IRewardService
+    {
+        Task<List<Rewards>> GetRewardsBetweenDates(DateTime from, DateTime to);
+    }
+}
