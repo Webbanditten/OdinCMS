10c30de46ef009857989a078ef5f2993f4a60ad0
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2023-01-11 19:34:31 +0100

    feat: refactor housekeeping menu

--- Areas/Housekeeping/Helpers/HousekeepingAccess.cs
+++ Areas/Housekeeping/Helpers/HousekeepingAccess.cs
@@ -8,7 +8,7 @@ using KeplerCMS.Models;
 namespace KeplerCMS.Areas.Housekeeping.Helpers
 {
     public static class HousekeepingAccess { 
-        public static bool HasAccess(List<string> userFuses, List<Fuse> fuses)
+        public static bool HasAccess(IEnumerable<string> userFuses, IEnumerable<Fuse> fuses)
         {
             return fuses.Select(fuse => fuse.Description()).Any(fuse => userFuses.Contains(fuse.ToLower()));
         }
